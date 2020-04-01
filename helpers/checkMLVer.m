585ad66704e2b4a73b5c8dcc72b2a2e82677a55e
Pavel Roslovets <roslovets@exponenta.ru>
2020-04-01 11:18:22 +0300

    Integrate updater

--- helpers/checkMLVer.m
+++ helpers/checkMLVer.m
@@ -1,5 +1,5 @@
 function checkMLVer()
 %% Check MATLAB version
-if VirusPropagationDev.mlver() < 9.85
+if VirusPropagationDev.mlver() < 9.8
     warning('Project was developed in MATLAB R2020a and will not work properly in older releases');
 end
