45a1bef2ad9a6a82e5724dbd6da869857b44af9a
Pavel Roslovets <roslovets@exponenta.ru>
2020-04-01 11:18:36 +0300

    Update doc

--- install.m
+++
@@ -1,9 +0,0 @@
-function install
-% Generated with Toolbox Extender https://github.com/ETMC-Exponenta/ToolboxExtender
-dev = VirusPropagationDev;
-dev.test('', false);
-% Post-install commands
-cd('..');
-ext = VirusPropagationExtender;
-ext.doc;
-% Add your post-install commands below
